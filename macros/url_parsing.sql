cdc076187158f84f5b46ce0454cabfcd72a13cbd
mrdat <mrdat0194@gmail.com>
2023-09-22 11:53:29 +0700

    change source

---
+++ macros/url_parsing.sql
@@ -0,0 +1,19 @@
+{% macro extract_hostname_from_url(url) %}
+    REGEXP_EXTRACT({{ url }}, '(?:http[s]?://)?(?:www\\.)?(.*?)(?:(?:/|:)(?:.)*|$)')
+{% endmacro %}
+
+{% macro extract_query_string_from_url(url) %}
+    REGEXP_EXTRACT({{ url }}, '\\?(.+)')
+{% endmacro %}
+
+{% macro remove_query_parameters(url, parameters)%}
+REGEXP_REPLACE(REGEXP_REPLACE(REGEXP_REPLACE(REGEXP_REPLACE({{url}}, '(\\?|&)({{ parameters|join("|") }})=[^&]*', '\\1'), '\\?&+', '?'), '&+', '&'), '\\?$|&$', '')
+{% endmacro %}
+
+{% macro extract_page_path(url) %}
+REGEXP_EXTRACT({{url}}, '(?:\\w+:)?\\/\\/[^\\/]+([^?#]+)')
+{% endmacro %}
+
+{% macro extract_query_parameter_value(url, param) %}
+    REGEXP_EXTRACT( {{url}}, r'{{param}}=([^&|\?|#]*)'  )
+{% endmacro %}
