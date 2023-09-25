988eb37aee76ecb62c84355d741055eada8529e7
Peter <dat@kasatria.com>
2023-09-25 10:10:40 +0000

    don't need to change these variable, run dbt run --full-refresh

--- macros/combine_property_data.sql
+++ macros/combine_property_data.sql
@@ -21,7 +21,7 @@
             {%- set relations = dbt_utils.get_relations_by_pattern(schema_pattern=schema_name, table_pattern='events_intraday_%', database=var('project')) -%}
             {% for relation in relations %}
                 {%- set relation_suffix = relation.identifier|replace('events_intraday_', '') -%}
-                {%- if relation_suffix|int = earliest_shard_to_retrieve|int -%}
+                {%- if relation_suffix|int >= earliest_shard_to_retrieve|int -%}
                     CREATE OR REPLACE TABLE `{{var('project')}}.{{var('dataset')}}.events_intraday_{{relation_suffix}}{{property_id}}` CLONE `{{var('project')}}.analytics_{{property_id}}.events_intraday_{{relation_suffix}}`;
                 {%- endif -%}
             {% endfor %}
@@ -29,7 +29,7 @@
             {%- set relations = dbt_utils.get_relations_by_pattern(schema_pattern=schema_name, table_pattern='events_%', exclude='events_intraday_%', database=var('project')) -%}
             {% for relation in relations %}
                 {%- set relation_suffix = relation.identifier|replace('events_', '') -%}
-                {%- if relation_suffix|int = earliest_shard_to_retrieve|int -%}
+                {%- if relation_suffix|int >= earliest_shard_to_retrieve|int -%}
                     CREATE OR REPLACE TABLE `{{var('project')}}.{{var('dataset')}}.events_{{relation_suffix}}{{property_id}}` CLONE `{{var('project')}}.analytics_{{property_id}}.events_{{relation_suffix}}`;
                     DROP TABLE IF EXISTS `{{var('project')}}.{{var('dataset')}}.events_intraday_{{relation_suffix}}{{property_id}}`;
                 {%- endif -%}
